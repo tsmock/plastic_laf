1302387a0e75a46e9cc8f3ce43d2d98ef981d8a1
don-vip <vincent.privat@gmail.com>
2015-06-08 00:48:18 +0000

    [josm_plastic_laf] update to JGoodies 2.8 + fix sonar issues. Renamed package and main plugin class

--- src/org/openstreetmap/josm/plugins/plasticlaf/PlasticLafPlugin.java
+++ src/org/openstreetmap/josm/plugins/plasticlaf/PlasticLafPlugin.java
@@ -1,9 +1,10 @@
-//License: GPL
-package plastic_laf;
+// License: GPL. For details, see LICENSE file.
+package org.openstreetmap.josm.plugins.plasticlaf;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 
 import com.jgoodies.looks.plastic.Plastic3DLookAndFeel;
@@ -11,16 +12,17 @@ import com.jgoodies.looks.plastic.PlasticLookAndFeel;
 import com.jgoodies.looks.plastic.PlasticXPLookAndFeel;
 
 /**
- * Plugin that brings JGoodies Plastic Look and Feel to JOSM.
+ * PlasticLafPlugin that brings JGoodies Plastic Look and Feel to JOSM.
  */
-public class Plugin {
+public class PlasticLafPlugin extends Plugin {
 
     /**
-     * Constructs a new {@code Plugin}.
+     * Constructs a new {@code PlasticLafPlugin}.
      * @param info plugin info
      * @throws UnsupportedLookAndFeelException if look and feel cannot be set
      */
-    public Plugin(PluginInformation info) throws UnsupportedLookAndFeelException {
+    public PlasticLafPlugin(PluginInformation info) throws UnsupportedLookAndFeelException {
+        super(info);
         UIManager.getDefaults().put("ClassLoader", getClass().getClassLoader());
         UIManager.installLookAndFeel("Plastic", PlasticLookAndFeel.class.getName());
         UIManager.installLookAndFeel("Plastic3D", Plastic3DLookAndFeel.class.getName());
